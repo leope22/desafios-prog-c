cd458286e6f8ce79fa6dab740d276ee38a2b733b
Leonardo Pedro <106115626+leope22@users.noreply.github.com>
2024-06-30 14:54:47 -0300

    Update matriz_2.c

--- matriz_2.c
+++ matriz_2.c
@@ -15,48 +15,49 @@ resposta “Sim” ou “Não”, dependendo se B é a inversa multiplicativa de
 
 #include <stdio.h>
 
-int main(){
-	long long int tam;
-	
-	scanf("%lld", &tam);
-	
-	long long int matriz1[tam][tam];
-	long long int matriz2[tam][tam];
-	long long int matriz3[tam][tam];
-	
-	for(long long int i = 0; i < tam; i++){
-		for(long long int j = 0; j < tam; j++){ 
-			matriz3[i][j] = 0;
-		}
+int main() {
+    long long int tam;
+
+    scanf("%lld", &tam);
+
+    long long int matriz1[tam][tam];
+    long long int matriz2[tam][tam];
+    long long int matriz3[tam][tam];
+
+    for (long long int i = 0; i < tam; i++) {
+        for (long long int j = 0; j < tam; j++) {
+            matriz3[i][j] = 0;
+        }
     }
-				         
-	for(long long int i = 0; i < tam; i++){
-		for(long long int j = 0; j < tam; j++){ 
-			scanf("%lld", &matriz1[i][j]);
-		}
+
+    for (long long int i = 0; i < tam; i++) {
+        for (long long int j = 0; j < tam; j++) {
+            scanf("%lld", &matriz1[i][j]);
+        }
     }
-    
-    for(long long int i = 0; i < tam; i++){
-		for(long long int j = 0; j < tam; j++){ 
-			scanf("%lld", &matriz2[i][j]);
-		}
+
+    for (long long int i = 0; i < tam; i++) {
+        for (long long int j = 0; j < tam; j++) {
+            scanf("%lld", &matriz2[i][j]);
+        }
     }
-    
-    for(long long int i = 0; i < tam; i++){
-		for(long long int j = 0; j < tam; j++){
-			for(long long int k = 0; k < tam; k++){				
-			    matriz3[i][j] += (matriz1[i][k] * matriz2[k][j]);
-		    }
-		    if((i != j) && (matriz3[i][j] != 0)){
-				printf("Nao\n");
-				return 0;
-			}else if((i == j) && (matriz3[i][j] != 1)){
-				printf("Nao\n");
-				return 0;
-            }else if((i == tam - 1) && (j == tam - 1) && (matriz3[i][j] == 1)){
-				printf("Sim\n");
-			}
-		}
-	}
+
+    for (long long int i = 0; i < tam; i++) {
+        for (long long int j = 0; j < tam; j++) {
+            for (long long int k = 0; k < tam; k++) {
+                matriz3[i][j] += (matriz1[i][k] * matriz2[k][j]);
+            }
+            if ((i != j) && (matriz3[i][j] != 0)) {
+                printf("Nao\n");
+                return 0;
+            } else if ((i == j) && (matriz3[i][j] != 1)) {
+                printf("Nao\n");
+                return 0;
+            } else if ((i == tam - 1) && (j == tam - 1) && (matriz3[i][j] == 1)) {
+                printf("Sim\n");
+            }
+        }
+    }
+
     return 0;
 }
